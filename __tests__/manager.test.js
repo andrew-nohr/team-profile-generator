d451122b9257bec8442eda2fd0302f41b4ed6d26
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2022-04-25 19:49:50 -0700

    created test file for manager

---
+++ __tests__/manager.test.js
@@ -0,0 +1,11 @@
+const Manager = require('../lib/Manager.js');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Jacob Jay', '1237', 'jacobjay@test.com', '456');
+
+    expect(manager.getName()).toBe('Lenovo Lenny');
+    expect(manager.getId()).toBe('1237');
+    expect(manager.getEmail()).toBe('jacobjay@test.com');
+    expect(manager.getRole()).toBe('Manager');
+    expect(manager.officeNumber).toBe('456');
+})
